e33cad18cee63f3a4945a8afff1f1691fea15ac7
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2020-02-08 20:39:14 +0530

    Update and rename cssp.groovy to git.groovy

---
+++ git.groovy
@@ -0,0 +1,10 @@
+def getCodeFromGithub() {
+    def github_URL = 'https://github.com/cssp007/NetbeansMavenProject'
+        if (${github_URL} == 'https://github.com/cssp007/NetbeansMavenProject') {
+                 git ${github_URL}
+        }
+        else {
+                 echo "github URL is NOT match"
+        }
+    }
+return this
