31332f414dde74f506fe4e3e7b21a9f88d14c51b
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2020-02-08 21:52:54 +0530

    Update git.groovy

--- git.groovy
+++ git.groovy
@@ -1,7 +1,7 @@
 def getCodeFromGithub() {
     def github_URL = 'https://github.com/cssp007/NetbeansMavenProject'
-        if (${github_URL} == 'https://github.com/cssp007/NetbeansMavenProject') {
-                 git ${github_URL}
+        if ('${github_URL}' == 'https://github.com/cssp007/NetbeansMavenProject') {
+                 git '${github_URL}'
         }
         else {
                  echo "github URL is NOT match"
