cf65008b7d65f2671edfc5a11531828522ab0e39
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2020-02-08 21:44:16 +0530

    Create maven.groovy

---
+++ maven.groovy
@@ -0,0 +1,23 @@
+def mavenClean() {
+          withMaven(maven : 'maven') {
+          sh 'mvn clean'
+    }
+}
+
+def compileMaven() {
+    withMaven(maven : 'maven') {
+          sh 'mvn compile'
+    }
+}
+
+def testMaven() {
+    withMaven(maven : 'maven') {
+          sh 'mvn test'
+    }
+}
+
+def packageMaven() {
+    withMaven(maven : 'maven') {
+          sh 'mvn package'
+    }
+}
